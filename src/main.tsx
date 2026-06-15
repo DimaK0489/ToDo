12d2577d952c8c93d832553e82ebd576cd4a35e1
Dima <76776847+DimaK0489@users.noreply.github.com>
2026-06-15 21:30:06 +0300

    add package redux

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false, // Отключаем перезапрос при смене вкладки браузера
-      retry: 1, // Количество попыток перезапроса при ошибке
+      refetchOnWindowFocus: false,
+      retry: 1,
     },
   },
 });
